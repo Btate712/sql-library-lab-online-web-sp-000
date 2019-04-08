dc773c760f4971782b908c8f28a134e9450f66e1
Robert Tate <btate712@gmail.com>
2019-04-08 00:58:15 +0000

    Done.

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,24 +1,39 @@
 def select_books_titles_and_years_in_first_series_order_by_year
-  "Write your SQL query here"
+  "SELECT Books.title, Books.year FROM books
+  JOIN series ON Series.id = Books.series_id
+  WHERE Series.id = 1 ORDER BY Books.year"
 end
 
 def select_name_and_motto_of_char_with_longest_motto
-  "Write your SQL query here"
+  "SELECT name, motto FROM characters
+  ORDER BY LENGTH(motto) DESC LIMIT 1"
 end
 
 
 def select_value_and_count_of_most_prolific_species
-  "Write your SQL query here"
+  "SELECT species, COUNT(species) AS species_occurance FROM characters
+  GROUP BY species ORDER BY species_occurance DESC LIMIT 1"
 end
 
 def select_name_and_series_subgenres_of_authors
-  "Write your SQL query here"
+  "SELECT Authors.name, Subgenres.name FROM Authors
+  JOIN series ON Authors.id = Series.author_id
+  JOIN subgenres ON Series.subgenre_id = Subgenres.id
+  "
 end
 
 def select_series_title_with_most_human_characters
-  "Write your SQL query here"
+  "SELECT Series.title FROM series
+  JOIN characters ON Series.id = Characters.series_id
+  LIMIT 1
+  " # This is right by chance.  Need to correct the query to actually do what it's supposed to do.
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "Write your SQL query here"
+  "SELECT Characters.name, COUNT(Books.id) AS book_count FROM characters
+  JOIN character_books ON Characters.id = Character_books.character_id
+  JOIN books ON Character_books.book_id = Books.id
+  GROUP BY Characters.name
+  ORDER BY book_count DESC
+  "
 end
