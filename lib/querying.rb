0799c7795955bb1c9992aa726f954785250833da
Robert Tate <btate712@gmail.com>
2019-04-10 23:48:39 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -23,11 +23,18 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "SELECT Series.title FROM series
-  JOIN characters ON Series.id = Characters.series_id
-  LIMIT 1
-  " # This is right purely by chance.  Need to go back and correct the query
-    # to actually do what it's supposed to do.
+  "SELECT series.title
+    FROM series
+    JOIN books
+    ON books.series_id = series.id
+    JOIN character_books
+    ON character_books.book_id = books.id
+    JOIN characters
+    ON character_books.character_id = characters.id
+    WHERE characters.species = 'human'
+    GROUP BY series.title
+    ORDER BY COUNT(*) DESC
+    LIMIT 1"
 end
 
 def select_character_names_and_number_of_books_they_are_in
